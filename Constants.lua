dbc784ba18399aca81b6883ca9d8974be5b06a6f
Ben <2kewl4u@gmx.li>
2020-03-31 03:50:05 +0200

    added Zul'Gurub items

--- Constants.lua
+++ Constants.lua
@@ -3,14 +3,16 @@ local _, ns = ...;
 
 -- raids
 local RAID_MOLTEN_CORE = "Molten Core"
-local RAID_BLACKWING_LAIR = "Blackwing Lair"
 local RAID_ONYXIA = "Onyxia's Lair"
+local RAID_BLACKWING_LAIR = "Blackwing Lair"
+local RAID_ZUL_GURUB = "Zul'Gurub"
 local RAID_AHN_QIRAJ = "Ahn'Qiraj"
 local RAIDS = {
     [RAID_MOLTEN_CORE] = true,
-    [RAID_BLACKWING_LAIR] = true,
     [RAID_ONYXIA] = true,
-    [RAID_AHN_QIRAJ] = true
+    [RAID_BLACKWING_LAIR] = true,
+    [RAID_ZUL_GURUB] = true,
+--    [RAID_AHN_QIRAJ] = true
 }
 
 -- add specific constants to the namespace
