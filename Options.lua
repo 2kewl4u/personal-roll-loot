d7c1db6d694aff3bfae5a0ab21ad9882347bd2a9
Ben <2kewl4u@gmx.li>
2020-04-19 11:25:26 +0200

    added option to roll and assign junk items

--- Options.lua
+++ Options.lua
@@ -3,12 +3,15 @@ local _, ns = ...;
 
 local Options = {
     -- indicates whether newly added players should be automatically marked as trial
-    markNewPlayersAsTrial
+    markNewPlayersAsTrial,
+    -- if junk items should be automatically rolled between raid or party members 
+    rollJunkItems
 }
 ns.Options = Options
 
 function Options.new()
     local self = setmetatable({}, Options)
     self.markNewPlayersAsTrial = true
+    self.rollJunkItems = false
     return self
 end
