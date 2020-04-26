05dba06a99fcaffb5dd424d9f0b6ffe52779302f
Ben <2kewl4u@gmx.li>
2020-04-26 17:45:19 +0200

    moved junk roll feature into the RollSystem

--- PersonalRollLoot.lua
+++ PersonalRollLoot.lua
@@ -7,6 +7,7 @@ local Items = ns.Items
 local Players = ns.Players
 local Raids = ns.Raids
 local Roles = ns.Roles
+local RollSystem = ns.RollSystem
 local utils = ns.utils
 
 local MasterUI = ns.MasterUI
@@ -216,47 +217,6 @@ local function inspectTarget()
     end
 end
 
-local function rollJunkItems()
-    -- check that master loot is enabled and the player is the master looter
-    if (ns.DB.options.rollJunkItems and
-        Raids.isInRaidInstance() and
-        utils.isMasterLooter(UnitName("player"))
-        ) then
-        
-        local lootCount = GetNumLootItems()
-        for slotIndex = 1, lootCount do
-            if (LootSlotHasItem(slotIndex)) then
-                local itemLink = GetLootSlotLink(slotIndex)
-                if (itemLink) then
-                    local itemId = GetItemInfoInstant(itemLink)
-                    -- filter junk items
-                    if (Items.isJunk(itemId)) then
-                        local lootIcon, lootName, lootQuantity, _, rarity, locked,
-                            isQuestItem, questId, isActive = GetLootSlotInfo(slotIndex)
-                        -- we roll out greens and blues only
-                        if (rarity == 2 or rarity == 3) then
-                            local candidates = {}
-                            for raidIndex = 1, 50 do
-                                local candidate = GetMasterLootCandidate(slotIndex, raidIndex)
-                                if (candidate) then
-                                    table.insert(candidates, raidIndex)
-                                else
-                                    break -- no more candidates
-                                end
-                            end
-                            if (#candidates > 0) then
-                                local pos = math.random(1, #candidates)
-                                local rndIndex = candidates[pos]
-                                GiveMasterLoot(slotIndex, rndIndex)
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
-end
-
 -- for testing
 local test
 SLASH_PersonalRollLootTest1 = "/test"
@@ -278,7 +238,7 @@ function eventFrame:OnEvent(event, arg1, arg2, arg3, arg4, arg5, ...)
         ns.loadSavedVariables()
     elseif (event == "LOOT_OPENED" or event == "LOOT_SLOT_CLEARED") then
         updateLootItems()
-        rollJunkItems()
+        RollSystem.rollJunkItems()
     elseif (event == "CHAT_MSG_LOOT") then
         -- arg5 "playerName2" - Name of player who received the loot
         parseLootMessage(arg1, arg5)
--- RollSystem.lua
+++ RollSystem.lua
@@ -1,6 +1,9 @@
 -- namespace
 local _, ns = ...;
 -- imports
+local Items = ns.Items
+local Raids = ns.Raids
+local utils = ns.utils
 
 local RollSystem = {
     -- the roll order for the current item distribution
@@ -19,4 +22,45 @@ function RollSystem.setRollOrder(rollOrder)
     -- prepare protocol
     RollSystem.sentIndex = 1
     RollSystem.responses = {}
+end
+
+function RollSystem.rollJunkItems()
+    -- check that master loot is enabled and the player is the master looter
+    if (ns.DB.options.rollJunkItems and
+        Raids.isInRaidInstance() and
+        utils.isMasterLooter(UnitName("player"))
+        ) then
+        
+        local lootCount = GetNumLootItems()
+        for slotIndex = 1, lootCount do
+            if (LootSlotHasItem(slotIndex)) then
+                local itemLink = GetLootSlotLink(slotIndex)
+                if (itemLink) then
+                    local itemId = GetItemInfoInstant(itemLink)
+                    -- filter junk items
+                    if (Items.isJunk(itemId)) then
+                        local lootIcon, lootName, lootQuantity, _, rarity, locked,
+                            isQuestItem, questId, isActive = GetLootSlotInfo(slotIndex)
+                        -- we roll out greens and blues only
+                        if (rarity == 2 or rarity == 3) then
+                            local candidates = {}
+                            for raidIndex = 1, 50 do
+                                local candidate = GetMasterLootCandidate(slotIndex, raidIndex)
+                                if (candidate) then
+                                    table.insert(candidates, raidIndex)
+                                else
+                                    break -- no more candidates
+                                end
+                            end
+                            if (#candidates > 0) then
+                                local pos = math.random(1, #candidates)
+                                local rndIndex = candidates[pos]
+                                GiveMasterLoot(slotIndex, rndIndex)
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
 end
